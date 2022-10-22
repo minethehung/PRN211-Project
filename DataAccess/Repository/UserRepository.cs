823d67941e29ad86ced50358e7f8b27f7e98849e
mai xuan quan <maixuanquan.mxq@gmail.com>
2022-10-22 16:33:14 +0700

    add User Repos and User DAO

--- DataAccess/Repository/UserRepository.cs
+++ DataAccess/Repository/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,32 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Repository
 {
-    public class UserRepository
+    public class UserRepository:IUserRepository
     {
-
+        public List<UserObject> GetUsers()
+        {
+            UserDAO userDAO = new UserDAO();
+            return userDAO.GetUsers();
+        }
+        public UserObject GetUser(string username)
+        {
+            UserDAO userDAO = new UserDAO();
+            return userDAO.GetUser(username);
+        }
+        public void UpdateUser(UserObject user)
+        {
+            UserDAO userDAO = new UserDAO();
+            userDAO.UpdateUser(user);
+        }
+        public void DeleteUser(string username)
+        {
+            UserDAO userDAO = new UserDAO();
+            userDAO.DeleteUser(username);
+        }
+        public void InsertNewUser(UserObject user)
+        {
+            UserDAO userDAO = new UserDAO();
+            userDAO.InsertNewUser(user);
+        }
     }
 }
