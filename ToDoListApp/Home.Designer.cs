ef18a16f72cc9fb145d2a2dda7c1e4ba9fc74f85
minethehung <hung.mai.150302@gmail.com>
2022-10-23 17:24:28 +0700

    commit vui ve

--- ToDoListApp/Home.Designer.cs
+++ ToDoListApp/Home.Designer.cs
@@ -37,6 +37,10 @@
             this.btnLogout = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btbSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnAllTask = new System.Windows.Forms.Button();
+            this.btnMyDate = new System.Windows.Forms.Button();
             this.btnDone = new System.Windows.Forms.Button();
             this.btnImportant = new System.Windows.Forms.Button();
             this.btnView = new System.Windows.Forms.Button();
@@ -61,7 +65,7 @@
             this.taskList.RowHeadersWidth = 51;
             this.taskList.RowTemplate.Height = 29;
             this.taskList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.taskList.Size = new System.Drawing.Size(680, 358);
+            this.taskList.Size = new System.Drawing.Size(482, 358);
             this.taskList.TabIndex = 38;
             this.taskList.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.taskList_CellClick);
             // 
@@ -71,7 +75,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.label1.Location = new System.Drawing.Point(0, 464);
+            this.label1.Location = new System.Drawing.Point(-99, 464);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(1145, 82);
             this.label1.TabIndex = 34;
@@ -83,7 +87,7 @@
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.Firebrick;
-            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Location = new System.Drawing.Point(-99, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(1145, 84);
             this.label2.TabIndex = 44;
@@ -102,9 +106,9 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(33, -2);
+            this.pictureBox1.Location = new System.Drawing.Point(33, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(144, 138);
+            this.pictureBox1.Size = new System.Drawing.Size(160, 155);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 44;
             this.pictureBox1.TabStop = false;
@@ -113,17 +117,18 @@
             // 
             this.labelUsername.AutoSize = true;
             this.labelUsername.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.labelUsername.Location = new System.Drawing.Point(33, 148);
+            this.labelUsername.Location = new System.Drawing.Point(33, 161);
             this.labelUsername.Name = "labelUsername";
             this.labelUsername.Size = new System.Drawing.Size(160, 31);
             this.labelUsername.TabIndex = 45;
             this.labelUsername.Text = "Name of User";
+            this.labelUsername.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(33, 197);
+            this.btnLogout.Location = new System.Drawing.Point(33, 201);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(144, 29);
+            this.btnLogout.Size = new System.Drawing.Size(160, 29);
             this.btnLogout.TabIndex = 46;
             this.btnLogout.Text = "Log Out";
             this.btnLogout.UseVisualStyleBackColor = true;
@@ -131,7 +136,7 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(21, 194);
+            this.btnExit.Location = new System.Drawing.Point(21, 302);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(202, 29);
             this.btnExit.TabIndex = 47;
@@ -142,19 +147,60 @@
             // panel2
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.btbSearch);
+            this.panel2.Controls.Add(this.txtSearch);
+            this.panel2.Controls.Add(this.btnAllTask);
+            this.panel2.Controls.Add(this.btnMyDate);
             this.panel2.Controls.Add(this.btnDone);
             this.panel2.Controls.Add(this.btnImportant);
             this.panel2.Controls.Add(this.btnExit);
             this.panel2.Controls.Add(this.btnView);
             this.panel2.Controls.Add(this.btnCreate);
-            this.panel2.Location = new System.Drawing.Point(910, 92);
+            this.panel2.Location = new System.Drawing.Point(712, 92);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(235, 355);
             this.panel2.TabIndex = 46;
             // 
+            // btbSearch
+            // 
+            this.btbSearch.Location = new System.Drawing.Point(141, 13);
+            this.btbSearch.Name = "btbSearch";
+            this.btbSearch.Size = new System.Drawing.Size(82, 29);
+            this.btbSearch.TabIndex = 51;
+            this.btbSearch.Text = "Search";
+            this.btbSearch.UseVisualStyleBackColor = true;
+            this.btbSearch.Click += new System.EventHandler(this.btbSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(21, 13);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(114, 27);
+            this.txtSearch.TabIndex = 50;
+            // 
+            // btnAllTask
+            // 
+            this.btnAllTask.Location = new System.Drawing.Point(21, 89);
+            this.btnAllTask.Name = "btnAllTask";
+            this.btnAllTask.Size = new System.Drawing.Size(202, 29);
+            this.btnAllTask.TabIndex = 49;
+            this.btnAllTask.Text = "All task";
+            this.btnAllTask.UseVisualStyleBackColor = true;
+            this.btnAllTask.Click += new System.EventHandler(this.btnAllTask_Click);
+            // 
+            // btnMyDate
+            // 
+            this.btnMyDate.Location = new System.Drawing.Point(21, 268);
+            this.btnMyDate.Name = "btnMyDate";
+            this.btnMyDate.Size = new System.Drawing.Size(202, 29);
+            this.btnMyDate.TabIndex = 48;
+            this.btnMyDate.Text = "My Day";
+            this.btnMyDate.UseVisualStyleBackColor = true;
+            this.btnMyDate.Click += new System.EventHandler(this.btnMyDate_Click);
+            // 
             // btnDone
             // 
-            this.btnDone.Location = new System.Drawing.Point(21, 15);
+            this.btnDone.Location = new System.Drawing.Point(21, 128);
             this.btnDone.Name = "btnDone";
             this.btnDone.Size = new System.Drawing.Size(202, 29);
             this.btnDone.TabIndex = 45;
@@ -164,7 +210,7 @@
             // 
             // btnImportant
             // 
-            this.btnImportant.Location = new System.Drawing.Point(21, 120);
+            this.btnImportant.Location = new System.Drawing.Point(21, 233);
             this.btnImportant.Name = "btnImportant";
             this.btnImportant.Size = new System.Drawing.Size(202, 29);
             this.btnImportant.TabIndex = 44;
@@ -173,7 +219,7 @@
             // 
             // btnView
             // 
-            this.btnView.Location = new System.Drawing.Point(21, 85);
+            this.btnView.Location = new System.Drawing.Point(21, 198);
             this.btnView.Name = "btnView";
             this.btnView.Size = new System.Drawing.Size(202, 29);
             this.btnView.TabIndex = 43;
@@ -183,7 +229,7 @@
             // 
             // btnCreate
             // 
-            this.btnCreate.Location = new System.Drawing.Point(21, 50);
+            this.btnCreate.Location = new System.Drawing.Point(21, 163);
             this.btnCreate.Name = "btnCreate";
             this.btnCreate.Size = new System.Drawing.Size(202, 29);
             this.btnCreate.TabIndex = 42;
@@ -195,7 +241,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1145, 546);
+            this.ClientSize = new System.Drawing.Size(947, 546);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
@@ -210,6 +256,7 @@
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -228,5 +275,9 @@
         private Button btnView;
         private Button btnCreate;
         private Button btnDone;
+        private Button btnMyDate;
+        private Button btnAllTask;
+        private Button btbSearch;
+        private TextBox txtSearch;
     }
 }
--- ToDoListApp/Home.cs
+++ ToDoListApp/Home.cs
@@ -21,23 +21,26 @@ namespace Group9_Project
             {
                 pictureBox1.ImageLocation = User.ImagePath;
             }
+            
             LoadData();
-
+         
         }
+
+      
+
         private void SetDataSource(BindingSource source)
         {
             taskList.DataSource = null;
             taskList.DataSource = source;
             taskList.Columns[0].Visible = false;
             string status = taskList.SelectedCells[4].Value.ToString();
-            if (status == "Complete")
-            {
+            if (status == "Complete") {
                 btnDone.Text = "To doing";
             }
-            else
-            {
+            else {
                 btnDone.Text = "Done";
             }
+
         }
         private void LoadData()
         {
@@ -62,6 +65,9 @@ namespace Group9_Project
                     source = new BindingSource();
                     source.DataSource = data;
                     SetDataSource(source);
+                }
+                else {
+                    MessageBox.Show("You do not have any task", "Show task", MessageBoxButtons.OK);
                 }
             }
             catch (Exception ex)
@@ -170,6 +176,78 @@ namespace Group9_Project
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Chang task status", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnMyDate_Click(object sender, EventArgs e) {
+            try {
+                List<TaskObject> tasks = taskRepository.GetAllTaskOfUser(this.User.Username);
+                List<dynamic> data = new List<dynamic>();
+                foreach (TaskObject task in tasks) {
+                    //System.Diagnostics.Debug.WriteLine(task.StartDate.Date.ToString());
+                   // System.Diagnostics.Debug.WriteLine(DateTime.Now.Date.ToString());
+                    if (task.StartDate.Date.CompareTo(DateTime.Now.Date) == 0) {
+                        data.Add(new {
+                            TaskId = task.TaskId,
+                            Title = task.Title,
+                            DueDate = task.DueDate,
+                            Importance = task.CategoryId,
+                            Status = task.State
+                        });
+                    }
+                }
+                if (data.Count > 0) {
+                    source = null;
+                    source = new BindingSource();
+                    source.DataSource = data;
+                    SetDataSource(source);
+                }
+                else {
+                    MessageBox.Show("You do not have any task to day", "Show today task", MessageBoxButtons.OK);
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Show important task", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnAllTask_Click(object sender, EventArgs e) {
+            LoadData();
+        }
+
+        private void btbSearch_Click(object sender, EventArgs e) {
+            try {
+                string searchValue = txtSearch.Text.Trim();
+                if (searchValue.Length == 0) {
+                    throw new Exception("Search value is empty");
+                }
+                List<TaskObject> tasks = taskRepository.GetAllTaskOfUser(this.User.Username);
+                List<dynamic> data = new List<dynamic>();
+                foreach (TaskObject task in tasks) {
+                    //System.Diagnostics.Debug.WriteLine(task.StartDate.Date.ToString());
+                    // System.Diagnostics.Debug.WriteLine(DateTime.Now.Date.ToString());
+                    if (task.Title.Contains(searchValue)) {
+                        data.Add(new {
+                            TaskId = task.TaskId,
+                            Title = task.Title,
+                            DueDate = task.DueDate,
+                            Importance = task.CategoryId,
+                            Status = task.State
+                        });
+                    }
+                }
+                if (data.Count > 0) {
+                    source = null;
+                    source = new BindingSource();
+                    source.DataSource = data;
+                    SetDataSource(source);
+                }
+                else {
+                    MessageBox.Show("No task match your search value", "Search task", MessageBoxButtons.OK);
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Show important task", MessageBoxButtons.OK);
             }
         }
     }
