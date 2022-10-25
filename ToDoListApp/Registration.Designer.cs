a6e528f39afec899b06a1eef0ad4475c2757a18c
mai xuan quan <maixuanquan.mxq@gmail.com>
2022-10-25 09:01:58 +0700

    registration color

--- ToDoListApp/Registration.Designer.cs
+++ ToDoListApp/Registration.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registration));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -44,66 +45,76 @@
             this.pBImage = new System.Windows.Forms.PictureBox();
             this.btnAddImg = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pBImage)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(56, 121);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(37, 76);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 31);
+            this.label1.Size = new System.Drawing.Size(135, 32);
             this.label1.TabIndex = 0;
             this.label1.Text = "User Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(56, 157);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(37, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 31);
+            this.label2.Size = new System.Drawing.Size(115, 32);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(56, 242);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(37, 206);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(117, 31);
+            this.label3.Size = new System.Drawing.Size(124, 32);
             this.label3.TabIndex = 2;
             this.label3.Text = "Full Name";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(56, 285);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(37, 249);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(146, 31);
+            this.label4.Size = new System.Drawing.Size(160, 32);
             this.label4.TabIndex = 3;
             this.label4.Text = "Date Of Birth";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(264, 125);
+            this.txtUsername.Location = new System.Drawing.Point(204, 76);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(308, 27);
             this.txtUsername.TabIndex = 4;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(264, 161);
+            this.txtPassword.Location = new System.Drawing.Point(204, 121);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(308, 27);
             this.txtPassword.TabIndex = 5;
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(264, 246);
+            this.txtName.Location = new System.Drawing.Point(204, 206);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(308, 27);
             this.txtName.TabIndex = 6;
@@ -111,9 +122,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.label5.ForeColor = System.Drawing.Color.DeepSkyBlue;
-            this.label5.Location = new System.Drawing.Point(247, 35);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(146, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(255, 54);
             this.label5.TabIndex = 8;
@@ -131,7 +143,7 @@
             // 
             // txtConfirm
             // 
-            this.txtConfirm.Location = new System.Drawing.Point(264, 206);
+            this.txtConfirm.Location = new System.Drawing.Point(204, 166);
             this.txtConfirm.Name = "txtConfirm";
             this.txtConfirm.Size = new System.Drawing.Size(308, 27);
             this.txtConfirm.TabIndex = 11;
@@ -139,16 +151,18 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(56, 202);
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(37, 166);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(95, 31);
+            this.label6.Size = new System.Drawing.Size(101, 32);
             this.label6.TabIndex = 10;
             this.label6.Text = "Confirm";
             // 
             // dateTimeBirthDay
             // 
-            this.dateTimeBirthDay.Location = new System.Drawing.Point(264, 289);
+            this.dateTimeBirthDay.Location = new System.Drawing.Point(204, 249);
             this.dateTimeBirthDay.Name = "dateTimeBirthDay";
             this.dateTimeBirthDay.Size = new System.Drawing.Size(308, 27);
             this.dateTimeBirthDay.TabIndex = 12;
@@ -157,16 +171,18 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.Location = new System.Drawing.Point(56, 328);
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.Yellow;
+            this.label7.Location = new System.Drawing.Point(-146, 219);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(80, 31);
+            this.label7.Size = new System.Drawing.Size(85, 32);
             this.label7.TabIndex = 13;
             this.label7.Text = "Avatar";
             // 
             // pBImage
             // 
-            this.pBImage.Location = new System.Drawing.Point(447, 322);
+            this.pBImage.Location = new System.Drawing.Point(387, 282);
             this.pBImage.Name = "pBImage";
             this.pBImage.Size = new System.Drawing.Size(125, 62);
             this.pBImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -175,7 +191,7 @@
             // 
             // btnAddImg
             // 
-            this.btnAddImg.Location = new System.Drawing.Point(264, 333);
+            this.btnAddImg.Location = new System.Drawing.Point(204, 293);
             this.btnAddImg.Name = "btnAddImg";
             this.btnAddImg.Size = new System.Drawing.Size(94, 29);
             this.btnAddImg.TabIndex = 15;
@@ -193,36 +209,47 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtUsername);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnAddImg);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pBImage);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.dateTimeBirthDay);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.txtConfirm);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(82, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(533, 364);
+            this.panel1.TabIndex = 17;
+            // 
             // Registration
             // 
             this.AcceptButton = this.btnRegister;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.CancelButton = this.btnExit;
             this.ClientSize = new System.Drawing.Size(700, 450);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.btnAddImg);
-            this.Controls.Add(this.pBImage);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.dateTimeBirthDay);
-            this.Controls.Add(this.txtConfirm);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.btnRegister);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtName);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.Name = "Registration";
             this.Text = "Registration";
             this.Load += new System.EventHandler(this.Registration_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pBImage)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -244,5 +271,6 @@
         private PictureBox pBImage;
         private Button btnAddImg;
         private Button btnExit;
+        private Panel panel1;
     }
 }
